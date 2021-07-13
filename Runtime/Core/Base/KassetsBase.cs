50d804138893f102cd974fe1e6fcbdc0893fb530
Ripandy Adha <ripandy.adha@gmail.com>
2021-07-14 04:18:13 +0700

    feat : base class and interfaces

--- Runtime/Core/Base/KassetsBase.cs
+++ Runtime/Core/Base/KassetsBase.cs
@@ -1,8 +1,13 @@
+using System;
 using UnityEngine;
 
 namespace Kadinche.Kassets
 {
-    public class KassetsBase : ScriptableObject
+    public abstract class KassetsBase : ScriptableObject, ISerializationCallbackReceiver, IDisposable
     {
+        public virtual void OnBeforeSerialize() {}
+        public virtual void OnAfterDeserialize() {}
+        public abstract void Dispose();
+        private void OnDestroy() => Dispose();
     }
 }
