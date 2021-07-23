b32b6b70325202251117d244df4f4bc1dceea251
Ripandy Adha <ripandy.adha@gmail.com>
2021-07-24 05:06:15 +0700

    chore / refactor : restructure core

--- Runtime/Core/Base/KassetsBase.cs
+++ Runtime/Core/Base/KassetsBase.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Kadinche.Kassets
@@ -9,5 +10,83 @@ namespace Kadinche.Kassets
         public virtual void OnAfterDeserialize() {}
         public abstract void Dispose();
         protected virtual void OnDestroy() => Dispose();
+    }
+    
+    internal class Subscription : IDisposable
+    {
+        private Action _action;
+        private IList<IDisposable> _disposables;
+        
+        public Subscription(
+            Action action,
+            IList<IDisposable> disposables)
+        {
+            _action = action;
+            _disposables = disposables;
+        }
+
+        public void Invoke() => _action.Invoke();
+        
+        public void Dispose()
+        {
+            if (_disposables.Contains(this))
+            {
+                _disposables.Remove(this);
+            }
+            
+            _action = null;
+            _disposables = null;
+        }
+    }
+    
+    internal class Subscription<T> : IDisposable
+    {
+        private Action<T> _action;
+        private IList<IDisposable> _disposables;
+        
+        public Subscription(
+            Action<T> action,
+            IList<IDisposable> disposables)
+        {
+            _action = action;
+            _disposables = disposables;
+        }
+
+        public void Invoke(T param) => _action.Invoke(param);
+        
+        public void Dispose()
+        {
+            if (_disposables.Contains(this))
+            {
+                _disposables.Remove(this);
+            }
+
+            _action = null;
+            _disposables = null;
+        }
+    }
+
+    internal class CompositeDisposable : IDisposable
+    {
+        private readonly IList<IDisposable> _disposables = new List<IDisposable>();
+        public void Add(IDisposable disposable) => _disposables.Add(disposable);
+
+        public void Dispose()
+        {
+            _disposables.Dispose();
+        }
+    }
+
+    internal static class DisposableExtension
+    {
+        internal static void Dispose(this IList<IDisposable> disposables)
+        {
+            foreach (var disposable in disposables)
+            {
+                disposable?.Dispose();
+            }
+            
+            disposables.Clear();
+        }
     }
 }
