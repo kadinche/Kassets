73cd171a5e781337d9e154856aae075ae730c362
Ripandy Adha <ripandy.adha@gmail.com>
2021-10-26 14:55:26 +0900

    feat : RequestResponseUniTaskSample

---
+++ Samples~/RequestResponseUniTaskSamples/Scripts/ResponseSampleUniTask.cs
@@ -0,0 +1,39 @@
+using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Kadinche.Kassets.RequestResponseSystem.Sample
+{
+    public class ResponseSampleUniTask : MonoBehaviour
+    {
+        [SerializeField] private FloatRequestResponseEvent _dummyProcessRequestResponseEvent;
+
+#if KASSETS_UNITASK
+        private IDisposable _subscription;
+
+        private void Start()
+        {
+            _subscription = _dummyProcessRequestResponseEvent.SubscribeResponse(ProcessRequest);
+        }
+
+        private async UniTask<float> ProcessRequest(float requestValue)
+        {
+            var token = this.GetCancellationTokenOnDestroy();
+            var delay = Random.value * 5;
+            await UniTask.Delay(TimeSpan.FromSeconds(delay), cancellationToken: token);
+            return delay;
+        }
+
+        private void OnDestroy()
+        {
+            _subscription?.Dispose();
+        }
+#else
+        private void Start()
+        {
+            Debug.LogError("UniTask not found. Please import UniTask first");
+        }
+#endif
+    }
+}
