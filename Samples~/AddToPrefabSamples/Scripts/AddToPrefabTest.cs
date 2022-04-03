4b1ed44181d690ff157436931dd757c113ad5063
RIpandy Adha Yusuf <ripandy.adha@kadinche.com>
2022-04-04 07:35:12 +0900

    feat : sample for add SO to prefab/other SO

---
+++ Samples~/AddToPrefabSamples/Scripts/AddToPrefabTest.cs
@@ -0,0 +1,66 @@
+﻿using Kadinche.Kassets.Variable;
+using UnityEngine;
+
+namespace Kadinche.Kassets.Sample
+{
+    public class AddToPrefabTest : MonoBehaviour
+    {
+        [SerializeField] private IntVariable _someIntVariable;
+        [SerializeField] private StringVariable _someStringVariable;
+        [SerializeField] private SomeScriptableObject _someScriptableObject;
+        
+#if UNITY_EDITOR
+
+        // Add
+        [ContextMenu("Add Int Variable", false)]
+        private void AddIntVariable() => _someIntVariable = gameObject.Add<IntVariable>();
+
+        [ContextMenu("Add Int Variable", true)]
+        private bool AddIntVariableValidate() => gameObject.AddValidate<IntVariable>();
+        
+        [ContextMenu("Add String Variable", false)]
+        private void AddStringVariable() => _someStringVariable = gameObject.Add<StringVariable>();
+
+        [ContextMenu("Add String Variable", true)]
+        private bool AddStringVariableValidate() => gameObject.AddValidate<StringVariable>();
+
+        [ContextMenu("Add SomeScriptableObject", false)]
+        private void AddSomeScriptableObject() => _someScriptableObject = gameObject.Add<SomeScriptableObject>();
+
+        [ContextMenu("Add SomeScriptableObject", true)]
+        private bool AddSomeScriptableObjectValidate() => gameObject.AddValidate<SomeScriptableObject>();
+        
+        // Remove
+        [ContextMenu("Remove Int Variable", false)]
+        private void RemoveIntVariable()
+        {
+            gameObject.Remove<IntVariable>();
+            _someIntVariable = null;
+        }
+        
+        [ContextMenu("Remove Int Variable", true)]
+        private bool RemoveIntVariableValidate() => gameObject.RemoveValidate<IntVariable>();
+        
+        [ContextMenu("Remove String Variable", false)]
+        private void RemoveStringVariable()
+        {
+            gameObject.Remove<StringVariable>();
+            _someStringVariable = null;
+        }
+        
+        [ContextMenu("Remove String Variable", true)]
+        private bool RemoveStringVariableValidate() => gameObject.RemoveValidate<StringVariable>();
+        
+        [ContextMenu("Remove SomeScriptableObject", false)]
+        private void RemoveSomeScriptableObject()
+        {
+            gameObject.Remove<SomeScriptableObject>();
+            _someScriptableObject = null;
+        }
+        
+        [ContextMenu("Remove SomeScriptableObject", true)]
+        private bool RemoveSomeScriptableObjectValidate() => gameObject.RemoveValidate<SomeScriptableObject>();
+
+#endif
+    }
+}
---
+++ Samples~/AddToPrefabSamples/Scripts/SomeScriptableObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Kadinche.Kassets.Sample
+{
+    [CreateAssetMenu(fileName = "SomeScriptableObject", menuName = MenuHelper.DefaultOtherMenu + "SomeScriptableObject", order = 100)]
+    public class SomeScriptableObject : ScriptableObject
+    {
+        public int id;
+        public string someName;
+    }
+}
