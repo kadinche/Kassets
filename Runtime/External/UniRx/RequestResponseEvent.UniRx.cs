16878f191868a11924737d84491f4b98ccd3d489
Ripandy Adha <ripandy.adha@gmail.com>
2022-04-18 05:33:34 +0900

    feat : Subscribe to Request, Subscribe to Response

--- Runtime/External/UniRx/RequestResponseEvent.UniRx.cs
+++ Runtime/External/UniRx/RequestResponseEvent.UniRx.cs
@@ -4,9 +4,12 @@ using UniRx;
 
 namespace Kadinche.Kassets.RequestResponseSystem
 {
-    public abstract partial class RequestResponseEvent<TRequest, TResponse>
+    public abstract partial class RequestResponseEvent<TRequest, TResponse> : IObservable<TResponse>
     {
         private readonly Subject<object> _requestSubject = new Subject<object>();
+        private readonly Subject<TResponse> _responseSubject = new Subject<TResponse>();
+        
+        public IDisposable Subscribe(IObserver<TResponse> observer) => _responseSubject.Subscribe(observer);
     }
     
 #if KASSETS_UNITASK
@@ -17,9 +20,30 @@ namespace Kadinche.Kassets.RequestResponseSystem
             _requestSubject.OnNext(this);
         }
         
+        public void Response_UniRx(Func<TRequest, TResponse> responseFunc)
+        {
+            if (_requests.Count <= 0) return;
+
+            var request = _requests.Dequeue();
+            var response = responseFunc.Invoke(request.Item1);
+            request.Item2.Invoke(response);
+            responseValue = response;
+            _responseSubject.OnNext(response);
+        }
+        
         private IDisposable HandleSubscribe_UniRx(Func<TRequest, TResponse> responseFunc)
         {
             return _requestSubject.Subscribe(_ => Response(responseFunc));
+        }
+        
+        private IDisposable HandleSubscribeToResponse_UniRx(Action action)
+        {
+            return _responseSubject.Subscribe(_ => action.Invoke());
+        }
+        
+        private IDisposable HandleSubscribeToResponse_UniRx(Action<TResponse> action)
+        {
+            return _responseSubject.Subscribe(action);
         }
 
         private void Dispose_UniRx()
@@ -28,6 +52,7 @@ namespace Kadinche.Kassets.RequestResponseSystem
             _requests.Clear();
             responseSubscription?.Dispose();
             _requestSubject.Dispose();
+            _responseSubject.Dispose();
         }
     }
 #else
@@ -37,10 +62,26 @@ namespace Kadinche.Kassets.RequestResponseSystem
         {
             _requestSubject.OnNext(this);
         }
+
+        private void RaiseResponse(TResponse response)
+        {
+            responseValue = response;
+            _responseSubject.OnNext(response);
+        }
         
         private IDisposable HandleSubscribe(Func<TRequest, TResponse> responseFunc)
         {
             return _requestSubject.Subscribe(_ => Response(responseFunc));
+        }
+        
+        private IDisposable HandleSubscribeToResponse(Action action)
+        {
+            return _responseSubject.Subscribe(_ => action.Invoke());
+        }
+        
+        private IDisposable HandleSubscribeToResponse(Action<TResponse> action)
+        {
+            return _responseSubject.Subscribe(action);
         }
 
         public override void Dispose()
@@ -49,6 +90,7 @@ namespace Kadinche.Kassets.RequestResponseSystem
             _requests.Clear();
             responseSubscription?.Dispose();
             _requestSubject.Dispose();
+            _responseSubject.Dispose();
         }
     }
 #endif
