6fef96886c092f2ad90cdd56eca83d36235cf02c
Ripandy Adha <ripandy.adha@gmail.com>
2022-06-10 05:31:11 +0900

    fix : limit implemented object pool to unity 2021 or later only

--- Runtime/Base/ObjectPools/GameObjectPool.cs
+++ Runtime/Base/ObjectPools/GameObjectPool.cs
@@ -1,3 +1,5 @@
+#if UNITY_2021_1_OR_NEWER
+
 using UnityEngine;
 
 namespace Kadinche.Kassets.ObjectPool
@@ -20,3 +22,5 @@ namespace Kadinche.Kassets.ObjectPool
         protected override void OnDestroyPoolObject(GameObject go) => Destroy(go);
     }
 }
+
+#endif
