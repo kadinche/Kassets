d305b8977f52fc7b4f8dc88ed1ab510b2d294aa1
Ripandy Adha <ripandy.adha@gmail.com>
2022-10-24 20:50:04 +0900

    fix : Vector3Collection wrong type

--- Runtime/Base/Collections/Vector3Collection.cs
+++ Runtime/Base/Collections/Vector3Collection.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace Kadinche.Kassets.Collection
 {
     [CreateAssetMenu(fileName = "Vector3Collection", menuName = MenuHelper.DefaultCollectionMenu + "Vector3Collection")]
-    public class Vector3Collection : Collection<Vector2>
+    public class Vector3Collection : Collection<Vector3>
     {
     }
 }
