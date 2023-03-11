018ad0a161bc75dd6392ee9878b34bad14753028
Ripandy Adha <ripandy.adha@gmail.com>
2023-03-12 07:32:27 +0900

    feat : inspector json operation custom path/filename

--- Editor/Core/VariableEditor.cs
+++ Editor/Core/VariableEditor.cs
@@ -8,39 +8,101 @@ namespace Kadinche.Kassets.Variable
     [CanEditMultipleObjects]
     public class VariableEditor : TypedGameEventEditor
     {
-        public override void OnInspectorGUI()
+        private bool _showJsonFileOperation;
+        private readonly string _jsonOpLabel = "Json File Management";
+        
+        private readonly string[] _jsonPathType = new[] { "Data Path", "Persistent Data Path", "Custom" };
+        private int _selectedType;
+        private bool _defaultFilename = true;
+        private string _jsonPath;
+        private string _filename;
+        
+        private void OnValidate()
         {
-            base.OnInspectorGUI();
-            AddJsonOperationButton();
+            _showJsonFileOperation = false;
         }
 
-        private void AddJsonOperationButton()
+        protected override void AddCustomButtons()
         {
             if (!(target is IVariable variable)) return;
 
-            serializedObject.Update();
-            
-            GUILayout.BeginHorizontal();
-
             GUI.enabled = true;
-            if (GUILayout.Button("Save to Json"))
+            
+            _showJsonFileOperation = EditorGUILayout.Foldout(_showJsonFileOperation, _jsonOpLabel);
+
+            if (_showJsonFileOperation)
             {
-                variable.SaveToJson();
-                Debug.Log($"Saved {target.name} to json file.");
+                EditorGUI.indentLevel++;
+
+                _selectedType = EditorGUILayout.Popup(
+                    label: new GUIContent("Json Path Type"),
+                    selectedIndex: _selectedType,
+                    displayedOptions: _jsonPathType
+                );
+
+                _jsonPath = _selectedType switch
+                {
+                    2 => EditorGUILayout.TextField(_jsonPath),
+                    1 => Application.persistentDataPath,
+                    _ => Application.dataPath
+                };
+                
+                EditorGUILayout.BeginHorizontal();
+                
+                GUILayout.Space(EditorGUI.indentLevel * 15);
+                
+                GUILayout.Label("File Name", GUILayout.ExpandWidth(false));
+                
+                GUI.enabled = !_defaultFilename;
+                _filename = EditorGUILayout.TextField(_filename);
+                
+                GUI.enabled = true;
+                
+                _defaultFilename = EditorGUILayout.Toggle(_defaultFilename, GUILayout.MaxWidth(30));
+                GUILayout.Label("Default", GUILayout.ExpandWidth(false));
+                
+                if (_defaultFilename)
+                    _filename = $"{target.name}.json";
+                
+                EditorGUILayout.EndHorizontal();
+
+                EditorGUILayout.BeginHorizontal();
+
+                GUILayout.Space(EditorGUI.indentLevel * 15);
+                
+                if (GUILayout.Button("Save to Json"))
+                {
+                    variable.SaveToJson(_jsonPath, _filename);
+                    Debug.Log($"Saved {_filename} to {_jsonPath}.");
+                    
+                    AssetDatabase.Refresh();
+                }
+
+                if (GUILayout.Button("Load from Json"))
+                {
+                    if (variable.IsJsonFileExist(_jsonPath, _filename))
+                    {
+                        serializedObject.Update();
+
+                        variable.LoadFromJson(_jsonPath, _filename);
+                        Debug.Log($"Loaded {_filename} from {_jsonPath}");
+                        
+                        serializedObject.ApplyModifiedProperties();
+                    }
+                    else
+                    {
+                        Debug.Log($"Could not found file {_jsonPath}/{_filename}.");
+                    }
+                }
+
+                GUILayout.EndHorizontal();
+
+                EditorGUI.indentLevel--;
             }
 
-            GUI.enabled = variable.IsJsonFileExist();
-            if (GUILayout.Button("Load from Json"))
-            {
-                variable.LoadFromJson();
-                Debug.Log($"Loaded {target.name} from json file.");
-            }
+            GUILayout.Space(15);
             
-            GUILayout.EndHorizontal();
-            
-            serializedObject.ApplyModifiedProperties();
-            
-            AssetDatabase.Refresh();
+            base.AddCustomButtons();
         }
     }
 }
