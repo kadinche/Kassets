0d3b28806b270c5c39994f7c0474675362d24ff4
Ripandy Adha <ripandy.adha@gmail.com>
2024-02-25 05:20:51 +0900

    docs : add some note comments

--- Runtime/Core/KassetsCore.cs
+++ Runtime/Core/KassetsCore.cs
@@ -16,6 +16,9 @@ namespace Kadinche.Kassets
         private void OnEnable()
         {
 #if UNITY_EDITOR
+            // NOTE : Unsubscribe then Subscribe ensures the subscription to the editor events only once.
+            // Using flags does not work as expected when Domain Reload is disabled.
+            // Reference : https://stackoverflow.com/a/7065833
             UnsubscribeEditorEvents();
             SubscribeToEditorEvents();
 #endif
@@ -36,14 +39,15 @@ namespace Kadinche.Kassets
             EditorUtility.SetDirty(this);
             if (IsDomainReloadDisabled) return;
 #endif
-            Dispose();
+            // NOTE : Do not call Dispose when Domain Reload is disabled.
+            // When Domain Reload is disabled, Disposed object doesn't get re-instantiated.
+            Dispose(); 
             Application.quitting -= OnQuit;
         }
 
         public abstract void Dispose();
     
 #if UNITY_EDITOR
-        // Handling of special case when user decides to disable domain reload.
         private bool IsDomainReloadDisabled => EditorSettings.enterPlayModeOptionsEnabled &&
                                                EditorSettings.enterPlayModeOptions.HasFlag(EnterPlayModeOptions.DisableDomainReload);
         
