570741de6f6fffcba2ac5d6e610cd1425a174b18
Ripandy Adha <ripandy.adha@gmail.com>
2024-06-16 04:17:07 +0900

    feat: TransactionCore R3 integration

---
+++ Runtime/External/R3/TransactionCore.R3.cs
@@ -0,0 +1,95 @@
+#if KASSETS_R3
+using System;
+using R3;
+
+namespace Kadinche.Kassets.Transaction
+{
+    public abstract partial class TransactionCore<TRequest, TResponse>
+    {
+        private readonly Subject<object> _requestSubject = new Subject<object>();
+        private readonly Subject<TResponse> _responseSubject = new Subject<TResponse>();
+    }
+    
+#if KASSETS_UNITASK
+    public abstract partial class TransactionCore<TRequest, TResponse>
+    {
+        private void TryRespond_UniRx()
+        {
+            _requestSubject.OnNext(this);
+        }
+        
+        public void Response_UniRx(Func<TRequest, TResponse> responseFunc)
+        {
+            if (_requests.Count <= 0) return;
+
+            var request = _requests.Dequeue();
+            var response = responseFunc.Invoke(request.Item1);
+            request.Item2.Invoke(response);
+            responseValue = response;
+            _responseSubject.OnNext(response);
+        }
+        
+        private IDisposable HandleSubscribe_UniRx(Func<TRequest, TResponse> responseFunc)
+        {
+            return _requestSubject.Subscribe(_ => Response(responseFunc));
+        }
+        
+        private IDisposable HandleSubscribeToResponse_UniRx(Action action)
+        {
+            return _responseSubject.Subscribe(_ => action.Invoke());
+        }
+        
+        private IDisposable HandleSubscribeToResponse_UniRx(Action<TResponse> action)
+        {
+            return _responseSubject.Subscribe(action);
+        }
+
+        protected override void Dispose_UniRx()
+        {
+            _requestSubject.Dispose();
+            _responseSubject.Dispose();
+            base.Dispose_UniRx();
+        }
+    }
+#else
+    public abstract partial class TransactionCore<TRequest, TResponse>
+    {
+        private void TryRespond()
+        {
+            _requestSubject.OnNext(this);
+        }
+
+        private void RaiseResponse(TResponse response)
+        {
+            responseValue = response;
+            _responseSubject.OnNext(response);
+        }
+        
+        private IDisposable HandleSubscribe(Func<TRequest, TResponse> responseFunc)
+        {
+            return _requestSubject.Subscribe(_ => Response(responseFunc));
+        }
+        
+        private IDisposable HandleSubscribeToResponse(Action action)
+        {
+            return _responseSubject.Subscribe(_ => action.Invoke());
+        }
+        
+        private IDisposable HandleSubscribeToResponse(Action<TResponse> action)
+        {
+            return _responseSubject.Subscribe(action);
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            _requests.Clear();
+            responseSubscription?.Dispose();
+            _requestSubject.Dispose();
+            _responseSubject.Dispose();
+        }
+    }
+#endif
+}
+
+#endif
